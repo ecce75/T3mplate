6bf4d4a93a01f3c21a4e78f0af5016febc7997cd
Haider Mansoor <haider@bitfactory.io>
2023-06-04 20:51:19 +0300

    changes

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import cameraImg from '../../resources/camera.png';
+import personImg from '../../resources/person.png';
+import deviceImg from '../../resources/device.png';
 
 const Description = () => {
   return (
-    <div id='description-page' className='page-secondary'>Description</div>
+    <div id='description-page' className='page-secondary'>
+        <span className='instructions-heading'>Instructions</span>
+        <div className='instructions-container'>
+        <div className='main-intructions'>
+        <div className='all-ins'>ins 1</div>
+        <div className='disclaimer-container'>
+        <div className='camera-instruction'>
+        <span className='instruction-heading'>1</span>
+        <img className='camera-instruction-img' src={cameraImg} />
+        <span className='instruction'>Make sure your camera is on</span>
+        </div>
+        <div className='camera-instruction'>
+        <span className='instruction-heading'>2</span>
+        <img className='camera-instruction-img' src={personImg} />
+        <span className='instruction'>Make sure you are aligned with the camera</span>
+        </div>
+        <div className='camera-instruction'>
+        <span className='instruction-heading'>3</span>
+        <img className='camera-instruction-img' src={deviceImg} />
+        <span className='instruction'>Make sure you are not using an older device as you may experience some issues</span>
+        </div>
+        </div> 
+        </div>
+
+        <div className='video-container'>
+            kajsld
+        </div>
+
+        </div> 
+    </div>
   )
 }
 
