1f9b42173f4979104e3eafb446461d61c83c183d
jpurdepr <joosep.purdeprits@gmail.com>
2023-06-05 00:48:05 +0300

    fixed gif height

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -7,37 +7,35 @@ import myGif from "../../resources/latteral.gif";
 const Description = () => {
   return (
     <div id='description-page' className='page-secondary'>
-        <span className='instructions-heading'>Instructions</span>
-        <div className='instructions-container'>
+      <span className='instructions-heading'>Instructions</span>
+      <div className='instructions-container'>
         <div className='main-intructions'>
-        <div className='all-ins'>ins 1</div>
-        <div className='disclaimer-container'>
-        <div className='camera-instruction'>
-        <span className='instruction-heading'>1</span>
-        <img className='camera-instruction-img' src={cameraImg} />
-        <span className='instruction'>Make sure your camera is on</span>
-        </div>
-        <div className='camera-instruction'>
-        <span className='instruction-heading'>2</span>
-        <img className='camera-instruction-img' src={personImg} />
-        <span className='instruction'>Make sure you are aligned with the camera</span>
-        </div>
-        <div className='camera-instruction'>
-        <span className='instruction-heading'>3</span>
-        <img className='camera-instruction-img' src={deviceImg} />
-        <span className='instruction'>Make sure you are not using an older device as you may experience some issues</span>
-        </div>
-        </div> 
-        </div>
-
-        <div className='video-container'>
-          <div>
-            <img src={myGif} alt="LATTERAL RAISE"></img>
+          <div className='all-ins'>ins 1</div>
+          <div className='disclaimer-container'>
+            <div className='camera-instruction'>
+              <span className='instruction-heading'>1</span>
+              <img className='camera-instruction-img' src={cameraImg} alt="camera" />
+              <span className='instruction'>Make sure your camera is on</span>
+            </div>
+            <div className='camera-instruction'>
+              <span className='instruction-heading'>2</span>
+              <img className='camera-instruction-img' src={personImg} alt="person" />
+              <span className='instruction'>Make sure you are aligned with the camera</span>
+            </div>
+            <div className='camera-instruction'>
+              <span className='instruction-heading'>3</span>
+              <img className='camera-instruction-img' src={deviceImg} alt="device" />
+              <span className='instruction'>Make sure you are not using an older device as you may experience some issues</span>
+            </div>
           </div>
         </div>
 
+        <div className='video-container'>
+          <img src={myGif} alt="LATTERAL RAISE"></img>
+        </div>
 
-        </div> 
+
+      </div>
     </div>
   )
 }
